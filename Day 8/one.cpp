7a6fd19fb3800dcef1bcbbf3ab30bec99246391b
Asifur Rahman (Ashiq) <57610730+asif17r@users.noreply.github.com>
2025-05-26 01:39:27 +0600

    day 8 done

--- Day 8/one.cpp
+++ Day 8/one.cpp
@@ -1,16 +1,63 @@
 /*
-    Given an array of lenght n, find the maximum element in the array.
+Check if a given integer x exists in a vector. Print "Found" or "Not Found".
 
-    Input:
-    5
-    2 3 5 2 1
-    Output:
-    5
+Input format:
+First line contains two integers n and x, where n is the number of elements in the vector and x is the integer to check.
+Second line contains n integers, the elements of the vector.
 
-    Input:
-    7 
-    34 12 45 67 23 89 23
+Sample Input:
+5 9
+1 3 5 7 9
 
-    Output:
-    89
+Output:
+Found
+
+
+Sample Input:
+5 4
+1 3 5 7 9
+
+Output:
+Not Found
+
+Sample Input:
+9 5
+100 3 4 98 5 6 7 8 9
+
+Output:
+Found
 */
+
+#include <bits/stdc++.h>
+using vector = std::vector<int>;
+int main()
+{
+    vector a;
+    int n;
+    int x;
+    int d;
+    bool t;
+    std::cout << "input n" << "\n";
+    std::cin >> n;
+    std::cout << "input x" << "\n";
+    std::cin >> x;
+
+    for (int i =  0;i < n;i++){
+        std::cin >> d;
+        a.push_back(d);
+        if (x == a[i]){
+            t = true;
+            break;
+        }
+        else{
+            t = false;
+        }        
+    }
+    if (t == true){
+        std::cout << "FOUND";
+    }
+    else{
+        std::cout << "NOT FOUND";
+    }
+    
+}
