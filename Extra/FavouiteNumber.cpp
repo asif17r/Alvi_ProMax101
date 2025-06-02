eec8d4c96928dbfcabf1e4bf468d8aa8b4804b00
TechAlvi <techwithalvi@gmail.com>
2025-06-03 00:24:09 +0600

    Added 20 more mins of free time 😎😎😎😎😎

---
+++ Extra/FavouiteNumber.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+
+int main(){
+    int t;
+    std::cin >> t;
+    std::printf("Wow %d is my favourite number!", t); // also used printf which is new so 5 mins more time 😎😎😎
+}
