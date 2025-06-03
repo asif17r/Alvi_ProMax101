e7b415026c2b6eff91bec2d228ff178214f2c314
Asifur Rahman (Ashiq) <asifurrahmanpabel@gmail.com>
2025-06-04 00:44:41 +0600

    Update Matrix_input_output.cpp

--- Unsolved/Matrix_input_output.cpp
+++ Unsolved/Matrix_input_output.cpp
@@ -30,25 +30,3 @@ Your task:
   Output 3:
     10 20 30 40
 */
-#include <iostream>
-#include <vector>
-using namespace std;
-
-int main() {
-    int n, m;
-    cin >> n >> m;
-    vector<vector<int>> matrix(n, vector<int>(m));
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            cin >> matrix[i][j];
-        }
-    }
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            cout << matrix[i][j];
-            if (j < m - 1) cout << " ";
-        }
-        cout << endl;
-    }
-    return 0;
-}
