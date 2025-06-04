db4a2268cb9dd897af3f2bec8294e75cce8490cf
TechAlvi <techwithalvi@gmail.com>
2025-06-04 13:25:07 +0600

    Update Matrix_input_output.cpp

--- Unsolved/Matrix_input_output.cpp
+++ Unsolved/Matrix_input_output.cpp
@@ -30,3 +30,28 @@ Your task:
   Output 3:
     10 20 30 40
 */
+
+// ------------------------------------------------------ //
+// Is this good
+#include <bits/stdc++.h>
+
+int main() {
+    int n, m;
+
+    std::cin >> n >> m;
+    int arr[n][m];
+    for (int i = 0;i < n;i++) {
+        for (int j = 0;j < m;j++) {
+            std::cin >> arr[i][j];
+        }
+
+    }
+    for (int i = 0;i < n;i++) {
+        for (int j = 0;j < m;j++) {
+            std::cout << arr[i][j] << " ";
+        }
+        std::cout << std::endl;
+
+    }
+
+}
