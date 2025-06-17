303ebc482eb9a8e273671418f9e28bb9f4354db6
Asifur Rahman (Ashiq) <asifurrahmanpabel@gmail.com>
2025-06-17 16:17:21 +0600

    rev integer

---
+++ Unsolved/Reverse_integer.cpp
@@ -0,0 +1,36 @@
+/*
+Reverse Integer
+
+Given an integer n, first determine if it is odd or even. Then, reverse the digits of the integer and print the reversed order without any preceding zeros.
+
+Input 1:
+1234
+Output 1:
+Even
+4321
+*****************
+Input 2:
+1000
+Output 2:
+Even
+1
+*****************
+Input 3:
+13579
+Output 3:
+Odd
+97531
+*****************
+Input 4:
+0
+Output 4:
+Even
+0
+*****************
+Input 5:
+2001
+Output 5:
+Odd
+1002
+*****************
+*/
