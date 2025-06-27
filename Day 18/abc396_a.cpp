58067c4ffaaa23d2991c76ed10ad5c81fd52efa7
Asifur Rahman (Ashiq) <asifurrahmanpabel@gmail.com>
2025-06-27 17:43:33 +0600

    ds

---
+++ Day 18/abc396_a.cpp
@@ -0,0 +1,54 @@
+//Problem: https://atcoder.jp/contests/abc396/tasks/abc396_a
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+using pairlist = vector<pair<int, string>>;
+using ll = long long;
+using ld = long double;
+int main() {
+    ll d = 0;
+    ll o = 0;
+    ll n;
+    #ifdef ALVI
+
+        freopen("input.txt", "r", stdin);
+    #endif
+    ll t;
+    bool ans = false;
+
+    cin >> t;
+    for (ll i = 0;i < t;i++) {
+         // taking input of the number we will CALCULATE
+         // adding the number of n
+         // if o == current n d++
+        cin >> n;
+
+
+        if (o == n) {
+            d++;
+
+
+        }
+
+        else {
+            o = 0;
+            d = 0;
+            o += n;
+        }
+        if (d >= 2) {
+            ans = true;
+            continue;
+        }
+        // cout << "n: "<< n << " d:"<<" " << d << " o:"<< " "<< o << endl;
+
+    }
+    if (ans) {
+        cout << "Yes" << endl;
+    }
+    else {
+        cout << "No" << endl;
+    }
+
+}
